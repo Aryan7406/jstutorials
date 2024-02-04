a6116a84ed939497b60993fc8f39b6305a4e617e
Aryan Prakash <147849460+Aryan7406@users.noreply.github.com>
2024-02-04 16:07:33 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const contactNumber = 8691952099;
+let myName = 'Aryan Prakash';
+var emailId = 'aryan7406@gmail.com';
+myName = 9;
+console.log(typeof myName);
+console.table([contactNumber, myName, emailId]);
